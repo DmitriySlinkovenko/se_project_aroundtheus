635f64e99f06bfef1481472b5eabd3a448a42c5d
Dmitriy <dmitriy.slinkovenko@gmail.com>
2024-03-25 13:04:40 -0400

    Project 4 stage 4 markup 1

--- scripts/index.js
+++ scripts/index.js
@@ -28,11 +28,40 @@ const initialCards = [
 const profileEditButton = document.querySelector(".profile__edit-button");
 const profileEditModal = document.querySelector(".modal");
 const profileEditCloseButton = document.querySelector(".modal__close-button");
+const modalTitle = document.querySelector(".form__title");
+const modalSubtitle = document.querySelector(".form__subtitle");
+const profileName = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__subtitle");
+const saveButton = document.querySelector(".form__save-button");
+const cardTemplate = document.querySelector("#card__template").content;
+const cardElements = document.querySelector(".elements");
 
 profileEditButton.addEventListener("click", function () {
   profileEditModal.classList.add("modal_opened");
+  modalTitle.value = profileName.textContent;
+  modalSubtitle.value = profileDescription.textContent;
 });
 
-profileEditCloseButton.addEventListener("click", function () {
+profileEditCloseButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
   profileEditModal.classList.remove("modal_opened");
 });
+
+saveButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  profileName.textContent = modalTitle.value;
+  profileDescription.textContent = modalSubtitle.value;
+  profileEditModal.classList.remove("modal_opened");
+});
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  cardImage.src = data.link;
+  cardTitle.textContent = data.name;
+  cardImage.alt = data.name;
+  cardElements.append(cardElement);
+}
+
+initialCards.forEach(getCardElement);
